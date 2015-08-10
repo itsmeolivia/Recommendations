d4d111e87af180d69a1da7fea5e272207b5a5208
Olivia Nordquist <nolivia@umich.edu>
2015-08-10 12:26:11 -0700

    initial commit

---
+++ app/src/main/java/com/itsmeolivia/recommendations/api/Etsy.java
@@ -0,0 +1,9 @@
+package com.itsmeolivia.recommendations.api;
+
+/**
+ * Created by olivia on 8/5/15.
+ */
+public class Etsy {
+
+    private static final String API_KEY = "m7bp7m88i1rcqcbhrr0fben6";
+}
